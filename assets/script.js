6d87eaa29572bb3d40ac2beabad47a52a7b1e6ad
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-11-12 11:35:41 -0500

    city weather is now appearing in main container

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,58 @@
-const secretApiKey = process.env.OPENWEATHERMAP_API_KEY;
+const apiKey = '47796ae8ef8fbb32d8ea6ca2f61e0495';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 
 async function getCityWeather(city) {
     try {
-      const response = await fetch(`${apiUrl}?q=${city}&appid=${secretApiKey}`);
-      if (!response.ok) {
-        throw new Error('Npo network response');
-      }
-      const data = await response.json();
-      return data;
+        const response = await fetch(`${apiUrl}?q=${city}&appid=${apiKey}`);
+        if (!response.ok) {
+            throw new Error('Error fetching weather data: ' + response.statusText);
+        }
+        const data = await response.json();
+        if (!data || !data.city || !data.city.name) {
+            throw new Error('Invalid data received from the API');
+        }
+        return data; 
     } catch (error) {
-      console.error('Error fetching weather', error.message);
+        console.error('Error fetching city weather:', error.message);
+        throw error; 
     }
-  }
-
+}
 
 function updateWeather(data) {
-  
     const citySearchTermEl = $("#city-search-term");
     const cityCurrentDateEl = $("#city-current-date");
     const cityCurrentIconEl = $("#city-current-icon");
     const tempInputEl = $("#temp-input");
     const windInputEl = $("#wind-input");
     const humidityInputEl = $("#humidity-input");
-  
-    citySearchTermEl.text(data.name);
-    cityCurrentDateEl.text(dayjs.unix(data.dt).format('MMM D, YYYY'));
-    const icon = $(`<img src='https://openweathermap.org/img/wn/${data.weather[0].icon}.png' alt='${data.weather[0].description}'/>`);
+
+    citySearchTermEl.text(data.city.name);
+    cityCurrentDateEl.text(dayjs.unix(data.list[0].dt).format('MMM D, YYYY'));
+    const icon = $(`<img src='https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png' alt='${data.list[0].weather[0].description}'/>`);
     cityCurrentIconEl.empty().append(icon);
-    tempInputEl.text(`Temp: ${data.main.temp} °F`);
-    windInputEl.text(`Wind: ${data.wind.speed} MPH`);
-    humidityInputEl.text(`Humidity: ${data.main.humidity} %`);
-  
-    fiveDayForecast(data.coord.lat, data.coord.lon);
-  }
-  
+    tempInputEl.text(`Temp: ${data.list[0].main.temp} °F`);
+    windInputEl.text(`Wind: ${data.list[0].wind.speed} MPH`);
+    humidityInputEl.text(`Humidity: ${data.list[0].main.humidity} %`);
+
+    fiveDayForecast(data.city.coord.lat, data.city.coord.lon);
+}
+
 async function fiveDayForecast(lat, lon) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`);
+        const response = await fetch(`${apiUrl}?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`);
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok');
         }
         const fiveDayView = await response.json();
-    
+
         const currentDayDate = dayjs();
         const filteredDayDate = fiveDayView.list.filter(el => !currentDayDate.startOf("day").isSame(dayjs.unix(el.dt).startOf("day"), "day"));
-    
+
         const fiveDayBoxEl = $("#fiveDayBox");
         fiveDayBoxEl.empty();
-    
-        for (let i = 0; i < filteredDays.length; i += 8) {
-          const forecastCard = $(`
+
+        for (let i = 0; i < filteredDayDate.length; i += 8) {
+            const forecastCard = $(`
             <div class="col-xl col-lg col-md col-sm-12 col-xs-12 col-12 card">
               <h4>${dayjs.unix(filteredDayDate[i].dt).format('MMM D, YYYY')}</h4>
               <img src='https://openweathermap.org/img/wn/${filteredDayDate[i].weather[0].icon}.png' alt='${filteredDayDate[i].weather[0].description}'/>
@@ -59,9 +61,50 @@ async function fiveDayForecast(lat, lon) {
               <div>Humidity: ${filteredDayDate[i].main.humidity} % </div>
             </div>
           `);
-          fiveDayBoxEl.append(forecastCard);
+            fiveDayBoxEl.append(forecastCard);
         }
-      } catch (error) {
+    } catch (error) {
         console.error('Error fetching five-day forecast:', error.message);
-      }
-  }
+        throw error;
+    }
+}
+
+$("#user-form").on("submit", async function (e) {
+    e.preventDefault();
+    const cityInput = $("#city-input").val();
+
+    try {
+        const weatherCityData = await getCityWeather(cityInput);
+        updateWeather(weatherCityData);
+
+        const searchHistoryData = JSON.parse(localStorage.getItem('searchHistoryData')) || [];
+        if (!searchHistoryData.includes(cityInput)) {
+            searchHistoryData.push(cityInput);
+            localStorage.setItem('searchHistoryData', JSON.stringify(searchHistoryData));
+            displayCities();
+        }
+    } catch (error) {
+        console.error('An error occurred:', error.message);
+    }
+});
+
+function retreiveCity() {
+    const cities = localStorage.getItem("cities");
+    return cities ? JSON.parse(cities) : [];
+}
+
+function displayCity() {
+    const cityListEl = $("#cityList");
+    cityListEl.empty();
+
+    retreiveCity().forEach(city => {
+        const eachCityEl = $(`
+        <button class='col-12 btn btn-secondary' onclick="getCityWeather('${city}')">
+          ${city}
+        </button>
+      `);
+        cityListEl.append(eachCityEl);
+    });
+}
+
+displayCity();
