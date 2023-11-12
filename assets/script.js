48fc2a3acc460680f6f49e96c27db9df5317c77c
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-11-11 23:37:26 -0500

    revised js code for first function

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,19 @@
 const secretApiKey = process.env.OPENWEATHERMAP_API_KEY;
 const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 
-async function getWeather(city) {
+async function getCityWeather(city) {
+    try {
+      const response = await fetch(`${apiUrl}?q=${city}&appid=${secretApiKey}`);
+      if (!response.ok) {
+        throw new Error('Npo network response');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching weather', error.message);
+    }
+  }
+  
 
-}
-
-function updateWeather(data) {
-}
-
-function updateUI(data) {
-}
-
-function init() {
-}
+  
 
