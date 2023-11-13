3b5f51c2fff7c3c5c663a47cb9a7e9aa1075e23c
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-11-12 23:12:00 -0500

    added comments

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,26 @@
+// These are the variables that will be used to display the current weather
 const apiKey = '47796ae8ef8fbb32d8ea6ca2f61e0495';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 
+// This function will fetch the weather data from the API for the given city typed inside search field
 async function getCityWeather(city, state = '') {
     try {
         const url = state ? `${apiUrl}?q=${city},${state}&appid=${apiKey}&units=imperial` : `${apiUrl}?q=${city}&appid=${apiKey}&units=imperial`;
         const response = await fetch(url);
 
+        // Check if the response was successful
         if (!response.ok) {
             throw new Error('Error fetching weather data: ' + response.statusText);
         }
 
         const data = await response.json();
 
+        // Check if the data contains the expected structure
         if (!data || !data.city || !data.city.name) {
             throw new Error('Invalid data received from the API');
         }
 
+        // Return the weather data
         return data;
     } catch (error) {
         console.error('Error fetching city weather:', error.message);
@@ -23,7 +28,7 @@ async function getCityWeather(city, state = '') {
     }
 }
 
-
+// This code below will update the weather data on the page with the given data and will display the given data to their respective elements
 function updateWeather(data) {
     const citySearchTermEl = $("#city-search-term");
     const cityCurrentDateEl = $("#city-current-date");
@@ -32,6 +37,7 @@ function updateWeather(data) {
     const windInputEl = $("#wind-input");
     const humidityInputEl = $("#humidity-input");
 
+    // Retreive the data from the API and display it to the page with the given elements
     citySearchTermEl.text(data.city.name);
     cityCurrentDateEl.text(dayjs.unix(data.list[0].dt).format('MMM D, YYYY'));
     const icon = $(`<img src='https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png' alt='${data.list[0].weather[0].description}'/>`);
@@ -40,9 +46,11 @@ function updateWeather(data) {
     windInputEl.text(`Wind: ${data.list[0].wind.speed} MPH`);
     humidityInputEl.text(`Humidity: ${data.list[0].main.humidity} %`);
 
+    // Call the fiveDayForecast function to display the five day forecast
     fiveDayForecast(data.city.coord.lat, data.city.coord.lon);
 }
 
+// This function will fetch the five day forecast data from the API for the given city typed inside search field
 async function fiveDayForecast(lat, lon) {
     try {
         const response = await fetch(`${apiUrl}?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`);
@@ -51,12 +59,15 @@ async function fiveDayForecast(lat, lon) {
         }
         const fiveDayView = await response.json();
 
+        // Filter out the current day from the five day forecast
         const currentDayDate = dayjs();
         const filteredDayDate = fiveDayView.list.filter(el => !currentDayDate.startOf("day").isSame(dayjs.unix(el.dt).startOf("day"), "day"));
 
+        // Display the five day forecast to the page
         const fiveDayBoxEl = $("#forecast-cards");
         fiveDayBoxEl.empty(); // Clear the existing forecast cards
 
+        // Loop through the filtered data and display the five day forecast to the page
         for (let i = 0; i < filteredDayDate.length; i += 8) {
             const forecastCard = $(`
                 <div class="col-lg-2">
@@ -79,7 +90,7 @@ async function fiveDayForecast(lat, lon) {
     }
 }
 
-
+// This code below will retreive the city from the localStorage
 function retreiveCity() {
     const cities = localStorage.getItem('searchHistoryData');
     return cities ? JSON.parse(cities) : [];
@@ -97,6 +108,7 @@ $(document).ready(function () {
             console.error('An error occurred while fetching city weather:', error.message);
         }
     }
+
 
 function displayCity() {
     const cities = retreiveCity();
@@ -122,6 +134,7 @@ function displayCity() {
     });
 }
 
+// This code below will display the city weather when the user clicks on the search button
 $("#user-form").on("submit", async function (e) {
     e.preventDefault();
     console.log('Form submitted!');
@@ -129,6 +142,7 @@ $("#user-form").on("submit", async function (e) {
     const cityInput = $("#city-input").val();
     const stateInput = $("#state-input").val();
 
+    // Check if the user has entered a city
     try {
         const weatherDataWithState = await getCityWeather(cityInput, stateInput);
         console.log('Weather data with state:', weatherDataWithState);
@@ -138,14 +152,16 @@ $("#user-form").on("submit", async function (e) {
 
         updateWeather(weatherDataWithoutState);
 
+        // Update the search history data in localStorage
         const searchHistoryData = JSON.parse(localStorage.getItem('searchHistoryData')) || [];
         console.log('Search history data before update:', searchHistoryData);
 
+        // Check if the city is already in the search history
         if (!searchHistoryData.includes(cityInput)) {
             searchHistoryData.push(cityInput);
             localStorage.setItem('searchHistoryData', JSON.stringify(searchHistoryData));
             console.log('Search history data after update:', searchHistoryData);
-
+            // Call the displayCity function to display the city to the page
             displayCity();
         }
     } catch (error) {
